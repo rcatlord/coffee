d57af35592cec76ddbd6af939d8a304498e7d9c3
rcatlord <12719575+rcatlord@users.noreply.github.com>
2025-08-18 15:26:04 +0100

    HTML popups

--- script.py
+++ script.py
@@ -8,17 +8,20 @@ import folium
 df = pd.read_csv('data.csv')
 
 #%%
-""" m = folium.Map(location=[53.478213561808005,-2.244687230244309], zoom_start=13, tiles='cartodbpositron')
-for row in df.iterrows():
-    row_values = row[1]
-    location = [row_values['lat'], row_values['lon']]
-    popup = popup = '<strong>' + row_values['name'] + '</strong>'
-    marker = folium.CircleMarker(location=location, 
-                                 radius=5,
-                                 color='black', fill_color='brown',
-                                 popup=popup)
-    marker.add_to(m)
-m """
+map = folium.Map(location=[53.478213561808005,-2.244687230244309], zoom_start=15, tiles='CartoDB positron')
+for index, row in df.iterrows():
+    latitude = row['lat']
+    longitude = row['lon']
+    text = f"""<a href="{row['url']}" target="_blank">{row['name']}</a><br>{row['address']}"""
+
+    folium.Marker(
+        location=[latitude, longitude],
+        popup=folium.Popup(text, max_width=2650),
+        icon=folium.Icon(color='red', icon='mug-saucer', prefix='fa')
+    ).add_to(map)
+map.save('map.html')
+
+## Alternatively
 
 #%%
 sf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat), crs = 'EPSG:4326')
@@ -26,14 +29,24 @@ sf = sf[['name','address','url','geometry']]
 sf.head()
 
 # %%
+title = f'<h1 style="position:absolute;z-index:100000;left:30vw;">Coffee shops</h1>'
 map = sf.explore(
     tiles='CartoDB positron', 
-    color='brown', 
-    style_kwds={'color':'black', 'opacity':0.5, 'fillOpacity':0.1}, 
-    marker_kwds={'radius':5}
+    marker_type='marker',
+    marker_kwds={'icon': folium.map.Icon(color='red', icon='mug-saucer', prefix='fa')},
+    tooltip = False, 
+    popup=True,
+    popup_kwds={"aliases": ["Name", "Address", "Website"]}
 )
+map.get_root().html.add_child(folium.Element(title))
 map
 
 # %%
-map.save('map.html')
-
+map = folium.Map(location=[53.478213561808005,-2.244687230244309], zoom_start=13)
+folium.GeoJson(
+    sf,
+    popup=folium.GeoJsonPopup(
+        fields=['name','address','url'],
+        aliases=['Name','Address','Link'])
+).add_to(map)
+map
